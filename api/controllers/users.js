c73e679c3d5a1dccbb8a1a24214c9d074becca7c
Amit Gujar <amitgujar3342@gmail.com>
2022-01-04 14:12:03 +0530

    minor fixes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,98 +2,50 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-router.post("/signup", (req, res, next) => {
-    User.find({ email: req.body.email })
+const userLogin = (req, res, next) => {
+  User.findOne({ email: req.body.email })
     .exec()
-    .then(user => {
-        if (user.length >= 1) {
-            return res.status(422).json({
-                message: "Email already exists"
-            });
-        } else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                  return res.status(500).json({
-                    error: err,
-                  });
-                } else {
-                  const user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    email: req.body.email,
-                    password: hash,
-                  });
-                  user
-                  .save()
-                  .then(result => {
-                      console.log(result);
-                      res.status(201).json({
-                          message: "User created successfully."
-                      });
-                  })
-                  .catch(err => {
-                      console.log(err);
-                      res.status(500).json({
-                          error: err
-                      })
-                  });
-                }
-            });
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
         }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "21d",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-
-});
-
-
-
-
-
-const userLogin =  (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .exec()
-    .then(user => {
-        if(user.length < 1) {
-            return res.status(401).json({
-                message: "Auth failed"
-            });
-        }
-        bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if (err) {
-                return res.status(401).json({
-                    message: "Auth failed"
-                });
-            }
-            if(result) {
-                const token = jwt.sign({
-                    email: user.email,
-                    userId: user._id
-
-                }, process.env.JWT_KEY, 
-                {
-                    expiresIn: "21d"
-                });
-               return res.status(200).json({
-                   message: "Auth successful",
-                   token: token
-               }); 
-            }
-            res.status(401).json({
-                message: "Auth failed"
-            });
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })
 };
 
-
-
-
-
-
 module.exports = {
-    userLogin
+  userLogin,
 };
