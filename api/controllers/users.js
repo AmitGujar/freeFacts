7d9ae658a4462060912a1f8107b236e7fcae9cfa
Amit Gujar <amitgujar3342@gmail.com>
2022-01-04 14:01:36 +0530

    signup added

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,6 +2,51 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+router.post("/signup", (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length >= 1) {
+            return res.status(422).json({
+                message: "Email already exists"
+            });
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                  return res.status(500).json({
+                    error: err,
+                  });
+                } else {
+                  const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    email: req.body.email,
+                    password: hash,
+                  });
+                  user
+                  .save()
+                  .then(result => {
+                      console.log(result);
+                      res.status(201).json({
+                          message: "User created successfully."
+                      });
+                  })
+                  .catch(err => {
+                      console.log(err);
+                      res.status(500).json({
+                          error: err
+                      })
+                  });
+                }
+            });
+        }
+    });
+
+});
+
+
+
+
+
 const userLogin =  (req, res, next) => {
     User.findOne({ email: req.body.email })
     .exec()
